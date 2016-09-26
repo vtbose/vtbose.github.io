0b19bab2a0a83bf1219d2c825d68c696fd6f9a1d
vtbose <vtbose@gmail.com>
2016-09-25 21:01:13 -0500

    Add unit tests

---
+++ QueryTest/FilterTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QueryTest
+{
+    [TestClass]
+    public class FilterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
